6fcc33856edb2c6062ed4cbf69d0c3e7ae49ec51
Artem Klishch <artemklishch@gmail.com>
2021-03-08 20:29:50 +0200

    gdsfgsdfgsd

--- 19compapi/pr3/src/App.vue
+++ 19compapi/pr3/src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <!-- <user-data :user-name="userName" :age="age"></user-data> -->
+    <user-data :first-name="firstName" :last-name="lastName" :third-name="thirdName" :age="age"></user-data>
+    <!-- <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>-->
+    <!-- <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>-->
     <!-- <button @click="user.age = 34">Change the Age</button> -->
     <button @click="setNewAge">Change the Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Third Name" ref="lastNameInputRef" />
+      <button @click="setLastNameInput">Set Last Name</button>
+      <!-- <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />-->
+    </div>
   </section>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed, watch, provide } from 'vue';
+import UserData from './components/UserData';
 export default {
+  components: {
+    UserData
+  },
   setup() {
-    const uName = ref('Maximilian');
+    const firstname = ref('');
+    const lastname = ref('');
+    const thirdname = ref('');
+    const lastNameInputRef = ref(null);
+    // const uName = ref('Maximilian');
     const uAge = ref(30);
     const user = reactive({
       name: 'Maximilian',
@@ -21,17 +41,69 @@ export default {
       user.age = 35;
       uAge.value = 40;
     }
-    return { user: user, setNewAge: setNewAge, userName: uName, age: uAge };
+    function setFirstName(e) {
+      firstname.value = e.target.value;
+    }
+    function setLastName(e) {
+      lastname.value = e.target.value;
+    }
+    const uName = computed(function() {
+      return firstname.value + ' ' + lastname.value + ' ' + thirdname.value;
+    });
+    // uName.value = '56' // такое писать нельзя, эти данные только для чтения
+    // watch(uAge, function(newValue, oldValue) {
+    //   console.log('Old age: ' + oldValue);
+    //   console.log('New age: ' + newValue);
+    // });
+    watch([uAge, uName], function(newValue, oldValue) {
+      console.log('Old age: ' + oldValue[0]);
+      console.log('New age: ' + newValue[0]);
+    }); // в этому случае аргументы колбека выводят новео и старое всех значений, что переданы в массиве
+    function setLastNameInput() {
+      thirdname.value = lastNameInputRef.value.value;
+    }
+    provide('userAge', uAge);
+    return {
+      user: user,
+      setNewAge: setNewAge,
+      userName: uName, // computed передаем как есть, без .value
+      age: uAge,
+      setFirstName: setFirstName,
+      setLastName: setLastName,
+      firstName: firstname,
+      lastName: lastname,
+      setLastNameInput: setLastNameInput,
+      lastNameInputRef: lastNameInputRef,
+      thirdName: thirdname
+    };
   }
+
   // data() {
   //   return {
   //     userName: 'Maximilian',
-  //      age: 31
+  //      age: 31,
+  //    firstName: '',
+  //    lastName: ''
   //   };
   // },
   // methods:{
   //   setNewAge(){
   //     this.age = 32
+  //   }
+  // }
+  // computed:{
+  //    userName(){
+  //      return this.firstName + ' ' + this.lastName;
+  //    }
+  // }
+  // watch:{
+  //   age(val){
+  //     console.log(val)
+  //   }
+  // }
+  // provide(){
+  //   return {
+  //     age: this.age
   //   }
   // }
 };
---
+++ 19compapi/pr3/src/components/UserData.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue';
+export default {
+  //   props: ['userName', 'age']
+  props: ['firstName', 'lastName', 'thirdName'],
+  setup(props, context) {
+    const uName = computed(function() {
+      return props.firstName + ' ' + props.lastName + ' ' + props.thirdName;
+      // setup принимает props
+    });
+    console.log(context);
+    // context.emit('save-data', 1) // то же, что и this.$emit('save-data', 1)
+
+    const age = inject('userAge');
+
+    onBeforeMount(function() {
+      console.log('onBeforeMount');
+    });
+    onMounted(function() {
+      console.log('onMounted');
+    });
+    onBeforeUpdate(function() {
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(function() {
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(function() {
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(function() {
+      console.log('onUnmounted');
+    });
+
+    return {
+      userName: uName,
+      age: age
+    };
+  }
+  //   computed: {
+  //     userName() {
+  //       return this.firstName + ' ' + this.lastName + ' ' + this.thirdName;
+  //     }
+  //   }
+};
+</script>
