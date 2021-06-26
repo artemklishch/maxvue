9522cb6120492b507176d5a236cf6336c4c792e8
artemklishch <artemklishch@gmail.com>
2021-06-26 13:30:11 +0300

    fsdfgghdgfh

---
+++ 22V2toV3/router-v3/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import App from './App.vue';
+import WelcomeScreen from './pages/WelcomeScreen.vue';
+import UsersList from './pages/UsersList.vue';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        { path: '/', component: WelcomeScreen },
+        { path: '/users', component: UsersList },
+    ],
+})
+
+const app = createApp(App)
+app.use(router)
+router.isReady().then(() => {
+    app.mount("#app")
+})
